cf821a7e6c8ee5acd962be33b27b90ce5e07a767
skkodz <55278921+skkodz@users.noreply.github.com>
2025-01-20 13:46:31 +0000

    Update Get-TreeSize.ps1
    Allow sorting by output tree size. Defaults to ascending, showing the largest directory tree first.

--- Get-TreeSize.ps1
+++ Get-TreeSize.ps1
@@ -1,10 +1,12 @@
 ##################################################################################
 # Get-TreeSize.ps1
-# Version: 1.0
+# Version: 1.1
 # Author: S Kodz
-# Last Updated 25 Jan 2023 by S Kodz
+# Last Updated 20 Jan 2025 by S Kodz
 #
 # Version History
+#   Version 1.1 - 20 Jan 2025 by S Kodz
+#      Adding parameters allowing for sorting of output by size.
 #   Version 1.0 - 25 Jan 2023 by S Kodz
 #      Original Version
 #
@@ -17,10 +19,40 @@
 ##################################################################################
 
 Param( 
-  [Parameter(Mandatory=$false)] [string]$Path = '.'
+  [Parameter(Mandatory=$false)] 
+  [string]$Path = '.',
+
+  [Parameter(Mandatory=$false)] 
+  [ValidateSet("Ascending", "Descending")]
+  [string]$SortOrder = "Descending"
 )
-if ( -not (Test-Path -Path $Path -PathType Container ) ) {Throw "$($Path) is not a valid directory." }
+
+# Check if the provided path is valid
+if (-not (Test-Path -Path $Path -PathType Container)) {
+  Throw "$($Path) is not a valid directory."
+}
+
 Write-Host "Please wait while the directory sizes within directory '$Path' are calculated. This may take some time..."
+
+# Get all subdirectories
 $directory = Get-ChildItem -Path $Path -Directory
-foreach ($dir in $directory) {
-Get-ChildItem -Path $dir -Recurse | Measure-Object -Sum Length | Select-Object @{Name="Path"; Expression={$dir.FullName}},@{Name="Files"; Expression={$_.Count}},@{Name="Size (GB)"; Expression={$_.Sum / 1GB}}}
+
+# Collect size information for each directory
+$results = foreach ($dir in $directory) {
+  $sizeInfo = Get-ChildItem -Path $dir -Recurse | Measure-Object -Sum Length
+  [PSCustomObject]@{
+    Path = $dir.FullName
+    Files = $sizeInfo.Count
+    SizeGB = $sizeInfo.Sum / 1GB
+  }
+}
+
+# Sort the results by size
+if ($SortOrder -eq "Ascending") {
+  $results = $results | Sort-Object -Property SizeGB
+} else {
+  $results = $results | Sort-Object -Property SizeGB -Descending
+}
+
+# Output the results
+$results
